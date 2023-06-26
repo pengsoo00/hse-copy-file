c33b3252e39c08add4a5e40eb242aa07e08ec564
Fake-news-detection <91871702+Fake-news-detection@users.noreply.github.com>
2023-06-26 20:34:03 +0300

    .

--- src/test/java/hse/filecopy/FileCopyUtilityTest.java
+++ src/test/java/hse/filecopy/FileCopyUtilityTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FileCopyUtilityTest {
     @Test
     void copyFileTest() {
